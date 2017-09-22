df5f2bfa0088290ed19c3bfe2f3d6d522d965f2f
Reyzor12 <testaccwow001@gmail.com>
2017-09-22 09:49:25 +0300

    Lesson 5 finish

--- src/main/java/ru/testspring/main/Start.java
+++ src/main/java/ru/testspring/main/Start.java
@@ -1,8 +1,15 @@
 package ru.testspring.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.testspring.impl.robot.ModelT1000;
+
 public class Start {
 
     public static void main(String[] args){
-        
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("configuration/ApplicationContext.xml");
+        ModelT1000 robot = (ModelT1000)context.getBean("t1000");
+        robot.dance();
     }
 }
