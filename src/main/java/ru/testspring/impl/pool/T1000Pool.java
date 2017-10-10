fca281565af653e81d00763cf3e9b96a58aa95e5
Reyzor12 <testaccwow001@gmail.com>
2017-10-10 09:44:19 +0300

    lesson 16 homework Map and constructor

--- src/main/java/ru/testspring/impl/pool/T1000Pool.java
+++ src/main/java/ru/testspring/impl/pool/T1000Pool.java
@@ -4,23 +4,25 @@ import ru.testspring.interfaces.Robot;
 import ru.testspring.interfaces.RobotPool;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class T1000Pool implements RobotPool {
 
-    private Collection<Robot> robotCollection;
+    private Map<String,Robot> robotCollection;
 
-    public void setRobotCollection(Collection<Robot> robotCollection){
+    public T1000Pool(Map<String,Robot> robotCollection){
         this.robotCollection = robotCollection;
     }
 
-    public Collection<Robot> getRobotCollection() {
+    public Map<String,Robot> getRobotCollection() {
         return robotCollection;
     }
 
     public void action(){
         if(robotCollection == null) return;
-        for(Robot robot:robotCollection){
-            robot.action();
+        for(Map.Entry<String,Robot> entry: robotCollection.entrySet()){
+            System.out.println(entry.getKey());
+            entry.getValue().action();
         }
     }
 }
--- src/main/java/ru/testspring/interfaces/RobotPool.java
+++ src/main/java/ru/testspring/interfaces/RobotPool.java
@@ -1,7 +1,8 @@
 package ru.testspring.interfaces;
 
 import java.util.Collection;
+import java.util.Map;
 
 public interface RobotPool {
-    Collection<Robot> getRobotCollection();
+    Map<String,Robot> getRobotCollection();
 }
