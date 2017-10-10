13ebeeaeb43de98ef0c8caabe7949f8f519502ba
Reyzor12 <testaccwow001@gmail.com>
2017-10-10 12:32:18 +0300

    lesson 18 avtomaticheskoe svi]yazovanie autowired

--- src/main/java/ru/testspring/main/Start.java
+++ src/main/java/ru/testspring/main/Start.java
@@ -15,7 +15,7 @@ public class Start {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("configuration/ApplicationContext.xml");
 //        ModelT1000 robot = (ModelT1000)context.getBean("t1000");
-        T1000Pool t1000Pool = (T1000Pool) context.getBean("t1000GoldPool");
-        t1000Pool.showRobot();
+        Robot robot = (ModelT1000) context.getBean("t1000");
+        robot.action();
     }
 }
