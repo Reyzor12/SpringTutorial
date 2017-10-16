081622955cee70e30e2c32b4ae5e447b1c7ea156
Reyzor12 <testaccwow001@gmail.com>
2017-10-16 12:45:48 +0300

    lesson 26 home work aop with mehods returning args

--- src/main/java/ru/testspring/lessonaop/logger/MyLogger.java
+++ src/main/java/ru/testspring/lessonaop/logger/MyLogger.java
@@ -3,6 +3,9 @@ package ru.testspring.lessonaop.logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.Set;
+
 @Component
 public class MyLogger {
 
@@ -21,5 +24,23 @@ public class MyLogger {
        System.out.println("method end " + joinPoint.getSignature().toShortString() + ", time = " + time + "ms");
 
        return output;
+   }
+
+   public void print(Object obj){
+
+       System.out.println("Print info begin>>>");
+       if(obj instanceof Set){
+           Set set = (Set) obj;
+           for(Object object:set){
+               System.out.println(object);
+           }
+       } else if(obj instanceof Map){
+           Map map = (Map) obj;
+           for(Object object:map.keySet()){
+               System.out.println("key = " + object + ", " + map.get(object));
+           }
+       }
+       System.out.println("Pring info end<<<");
+       System.out.println();
    }
 }
